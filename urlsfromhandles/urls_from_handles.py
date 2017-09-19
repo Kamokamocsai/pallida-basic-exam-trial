7320cb25e9d13ea93f47677fa0fb471826e520a3
András Kamocsai <kamo.andras@gmail.com>
2017-09-19 10:47:24 +0200

    feat(urls_from_handles): works

--- urlsfromhandles/urls_from_handles.py
+++ urlsfromhandles/urls_from_handles.py
@@ -1,11 +1,11 @@
-# Create a function that takes a list of GitHub handles as input and returns a
-# list of strings that represents
-# GitHub url's under Green Fox Academy organization in the following format:
-# "https://github.com/greenfox-academy/teststudent"
+names = ["teststudent", "ghhandle2"]
 
-# example:
-# input: ["ghhandle1", "ghhandle2"]
-# output: ["https://github.com/greenfox-academy/ghhandle1", "https://github.com/greenfox-academy/ghhandle2"]
+def urls_from_handles(names):
+    output = []
+    full_url1 = "https://github.com/greenfox-academy/" + names[0]
+    full_url2 = "https://github.com/greenfox-academy/" + names[1]
+    output.append(full_url1)
+    output.append(full_url2)
+    print(output)
 
-names = ["ghhandle1", "ghhandle2"]
 print(urls_from_handles(names))
