e82c2bf23ea17d3832210b4d14df4eb849ef97cd
András Kamocsai <kamo.andras@gmail.com>
2017-09-19 10:01:52 +0200

    refact(name_from_email): refactor where change capital letter

--- namefromemail/name_from_email.py
+++ namefromemail/name_from_email.py
@@ -2,8 +2,8 @@ def name_from_email(email_adress):
     snail_mail = email_adress.split("@")
     splitted_names = snail_mail[0]
     full_name = splitted_names.split(".")
-    last_name = full_name[1]
-    first_name = full_name[0]
-    print(last_name.title(), first_name.title())
+    last_name = full_name[1].title()
+    first_name = full_name[0].title()
+    print(last_name, first_name)
 
 print(name_from_email("elek.viz@exam.com"))
