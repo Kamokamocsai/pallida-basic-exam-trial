3c264367683754b03e394ebba3fe76611e4c871d
András Kamocsai <kamo.andras@gmail.com>
2017-09-19 09:58:38 +0200

    feat(name_of_email): capitalize output names

--- namefromemail/name_from_email.py
+++ namefromemail/name_from_email.py
@@ -2,6 +2,8 @@ def name_from_email(email_adress):
     snail_mail = email_adress.split("@")
     splitted_names = snail_mail[0]
     full_name = splitted_names.split(".")
-    print(full_name[1], full_name[0])
+    last_name = full_name[1]
+    first_name = full_name[0]
+    print(last_name.title(), first_name.title())
 
 print(name_from_email("elek.viz@exam.com"))
