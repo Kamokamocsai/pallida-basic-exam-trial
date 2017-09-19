b1146f7fad5faec1538dfe49e323214adac1c8f8
András Kamocsai <kamo.andras@gmail.com>
2017-09-19 09:54:02 +0200

    feat(add_from_email): prints the correct sort of the name

--- namefromemail/name_from_email.py
+++ namefromemail/name_from_email.py
@@ -1,18 +1,7 @@
-# Create a function that takes email address as input in the following format:
-# firstName.lastName@exam.com
-# and returns a string that represents the user name in the following format:
-# last_name first_name
-# example: "elek.viz@exam.com" for this input the output should be: "Viz Elek"
-# accents does not matter
-
 def name_from_email(email_adress):
     snail_mail = email_adress.split("@")
-    print(snail_mail)
-    splitted_names = snail_mail.split(".")
-    print(splitted_names)
-    # index_mail = splitted_mail.index("@")
-    # splitted_mail.insert(index_mail, valami) 
-    # solution = " ".join(splitted_quote)
-
+    splitted_names = snail_mail[0]
+    full_name = splitted_names.split(".")
+    print(full_name[1], full_name[0])
 
 print(name_from_email("elek.viz@exam.com"))
